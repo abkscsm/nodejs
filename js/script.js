122a08a6e154a205bf8ba4a05aaa8cfa0f34a66c
abkscsm <abkscsm2409@gmail.com>
2022-03-30 12:31:41 +0530

    Execution context - example

---
+++ js/script.js
@@ -0,0 +1,22 @@
+//some_fun();
+var n = undefined;
+var x = undefined;
+var y = undefined;
+var z = undefined;
+
+
+document.getElementById("num1").innerHTML = "n =" + n;
+document.getElementById("num2").innerHTML = "x =" + x;
+document.getElementById("num3").innerHTML = "y =" + y;
+document.getElementById("num4").innerHTML = "z =" + z;
+var n = 10;
+var x = 21;
+var y = 22;
+var z = 111;
+/*function some_fun(){
+	document.getElementById("fun").innerHTML = 'Function invoked';
+}*/
+document.getElementById("afnum1").innerHTML = "n =" + n;
+document.getElementById("afnum2").innerHTML = "x =" + x;
+document.getElementById("afnum3").innerHTML = "y =" + y;
+document.getElementById("afnum4").innerHTML = "z =" + z;
