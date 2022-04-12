3c36cf76ba7e3358048126c4a569825f6f32edf5
abkscsm <abkscsm2409@gmail.com>
2022-04-12 11:14:16 +0530

    Variable scope and anonymus function.

---
+++ function-var-scope.js
@@ -0,0 +1,9 @@
+var num1 = 20, num2 = 21;
+var someFun = function(){
+	var add = num1 + num2;
+}
+someFun()
+function otherFun(){
+	console.log(add);
+}
+otherFun()
--- js/script.js
+++ js/script.js
@@ -1,22 +1,8 @@
-//some_fun();
-var n = undefined;
-var x = undefined;
-var y = undefined;
-var z = undefined;
-
-
-document.getElementById("num1").innerHTML = "n =" + n;
-document.getElementById("num2").innerHTML = "x =" + x;
-document.getElementById("num3").innerHTML = "y =" + y;
-document.getElementById("num4").innerHTML = "z =" + z;
-var n = 10;
-var x = 21;
-var y = 22;
-var z = 111;
-/*function some_fun(){
-	document.getElementById("fun").innerHTML = 'Function invoked';
-}*/
-document.getElementById("afnum1").innerHTML = "n =" + n;
-document.getElementById("afnum2").innerHTML = "x =" + x;
-document.getElementById("afnum3").innerHTML = "y =" + y;
-document.getElementById("afnum4").innerHTML = "z =" + z;
+//console.log(num);
+//console.log(print_name);
+var num = 21;
+var print_name = function(){
+	document.write("JavaScript")
+}
+console.log(num);
+print_name();
